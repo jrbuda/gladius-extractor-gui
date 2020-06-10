85ffdbed2ff1027b04529669418dbf4002c834c0
jrbuda <jrbuda01@gmail.com>
2020-06-10 12:37:24 -0400

    Version1.0
    Fully working version that will pack and unpack the data.bec files

--- src/main/java/gui.java
+++ src/main/java/gui.java
@@ -41,6 +41,8 @@ public class gui extends javax.swing.JFrame {
 
         jfcSourceFile = new javax.swing.JFileChooser();
         jfcOutput = new javax.swing.JFileChooser();
+        btnGrpTools = new javax.swing.ButtonGroup();
+        btnGrpMode = new javax.swing.ButtonGroup();
         txtSourceLoc = new javax.swing.JTextField();
         btnSourceLoc = new javax.swing.JButton();
         txtOutputLoc = new javax.swing.JTextField();
@@ -50,11 +52,14 @@ public class gui extends javax.swing.JFrame {
         jrbTok = new javax.swing.JRadioButton();
         jrbZlib = new javax.swing.JRadioButton();
         jLabel1 = new javax.swing.JLabel();
-        btnExtract = new javax.swing.JButton();
+        btnRunTool = new javax.swing.JButton();
+        jrbUnpack = new javax.swing.JRadioButton();
+        jrbPack = new javax.swing.JRadioButton();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        txtSourceLoc.setText("Source file lcoation here");
+        txtSourceLoc.setText("Source file location here");
         txtSourceLoc.setEnabled(false);
 
         btnSourceLoc.setText("Find file to be extracted");
@@ -79,26 +84,49 @@ public class gui extends javax.swing.JFrame {
             }
         });
 
+        btnGrpTools.add(jrbBec);
         jrbBec.setSelected(true);
         jrbBec.setText("bec-tool");
 
+        btnGrpTools.add(jrbPak);
         jrbPak.setText("pak-tool");
         jrbPak.setEnabled(false);
 
+        btnGrpTools.add(jrbTok);
         jrbTok.setText("tok-tool");
         jrbTok.setEnabled(false);
 
+        btnGrpTools.add(jrbZlib);
         jrbZlib.setText("zlib-tool");
         jrbZlib.setEnabled(false);
 
         jLabel1.setText("Select a tool below:");
 
-        btnExtract.setText("Extract files");
-        btnExtract.addActionListener(new java.awt.event.ActionListener() {
+        btnRunTool.setText("Run tool");
+        btnRunTool.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnExtractActionPerformed(evt);
+                btnRunToolActionPerformed(evt);
             }
         });
+
+        btnGrpMode.add(jrbUnpack);
+        jrbUnpack.setSelected(true);
+        jrbUnpack.setText("Unpack Mode");
+        jrbUnpack.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jrbUnpackActionPerformed(evt);
+            }
+        });
+
+        btnGrpMode.add(jrbPack);
+        jrbPack.setText("Pack Mode");
+        jrbPack.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jrbPackActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Select a mode below:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -107,49 +135,61 @@ public class gui extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(txtSourceLoc)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(btnExtract, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jrbTok)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jrbZlib))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jrbBec)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jrbPak))
-                            .addComponent(jLabel1))
-                        .addGap(0, 170, Short.MAX_VALUE))
-                    .addComponent(btnSourceLoc, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(txtOutputLoc)
-                    .addComponent(btnOutputLoc, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap())
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(jrbBec)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jrbPak))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(jrbTok)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jrbZlib))
+                                    .addComponent(jLabel1))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jrbPack)
+                                    .addComponent(jrbUnpack)))
+                            .addComponent(btnRunTool, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(138, 138, 138))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtSourceLoc)
+                            .addComponent(btnSourceLoc, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(txtOutputLoc)
+                            .addComponent(btnOutputLoc, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(txtSourceLoc)
+                .addComponent(txtSourceLoc, javax.swing.GroupLayout.DEFAULT_SIZE, 43, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnSourceLoc)
-                .addGap(18, 18, 18)
-                .addComponent(txtOutputLoc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(txtOutputLoc, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnOutputLoc)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jrbBec)
-                    .addComponent(jrbPak))
+                    .addComponent(jrbPak)
+                    .addComponent(jrbUnpack))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jrbTok)
-                    .addComponent(jrbZlib))
+                    .addComponent(jrbZlib)
+                    .addComponent(jrbPack))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnExtract, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(42, Short.MAX_VALUE))
+                .addComponent(btnRunTool, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
@@ -161,11 +201,19 @@ public class gui extends javax.swing.JFrame {
 
    
     private void btnSourceLocActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSourceLocActionPerformed
-        int returnVal = jfcSourceFile.showOpenDialog(this);
-        File file = jfcSourceFile.getSelectedFile();
-          // What to do with the file, e.g. display it in a TextArea
-        txtSourceLoc.setText(file.getAbsolutePath());
-        SourceLoc = file.getAbsolutePath();
+        if(jrbUnpack.isSelected() == true){
+            jfcSourceFile.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            int returnVal = jfcSourceFile.showOpenDialog(this);
+            File file = jfcSourceFile.getSelectedFile();
+            txtSourceLoc.setText(file.getAbsolutePath());
+            SourceLoc = file.getAbsolutePath();
+        } else if(jrbPack.isSelected() == true){
+            jfcSourceFile.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            int returnVal = jfcSourceFile.showOpenDialog(this);
+            File directory = jfcSourceFile.getSelectedFile();
+            txtSourceLoc.setText(directory.getAbsolutePath());
+            SourceLoc = directory.getAbsolutePath()+"/";
+        }
     }//GEN-LAST:event_btnSourceLocActionPerformed
 
     private void btnOutputLocActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOutputLocActionPerformed
@@ -176,9 +224,25 @@ public class gui extends javax.swing.JFrame {
         OutputLoc = directory.getAbsolutePath()+"/";
     }//GEN-LAST:event_btnOutputLocActionPerformed
 
-    private void btnExtractActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExtractActionPerformed
-        runBec(SourceLoc, OutputLoc);
-    }//GEN-LAST:event_btnExtractActionPerformed
+    private void btnRunToolActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRunToolActionPerformed
+        if(jrbUnpack.isSelected() == true){
+            unpackBec(SourceLoc, OutputLoc);
+        }else if(jrbPack.isSelected() == true){
+            packBec(SourceLoc, OutputLoc);
+        }else{
+            
+        }
+    }//GEN-LAST:event_btnRunToolActionPerformed
+
+    private void jrbPackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jrbPackActionPerformed
+        btnOutputLoc.setText("Choose bec output folder");
+        btnSourceLoc.setText("Find unpacked bec folder");
+    }//GEN-LAST:event_jrbPackActionPerformed
+
+    private void jrbUnpackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jrbUnpackActionPerformed
+        btnSourceLoc.setText("Find file to be extracted");
+        btnOutputLoc.setText("Choose output folder");
+    }//GEN-LAST:event_jrbUnpackActionPerformed
 
     /**
      * @param args the command line arguments
@@ -215,55 +279,91 @@ public class gui extends javax.swing.JFrame {
         });
     }
     
-    public void runBec(String SourceLoc, String OutputLoc){
-        /* Neither of these work?????
-        System.out.println("Starting runBec");
-        String location = gui.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        location = location.replaceAll("/", "\\\\");
-        Process process = null;
-        try{
-            //process = Runtime.getRuntime().exec(new String[]{"cmd.exe python "+location + "bec-tool.py","-unpack", SourceLoc, OutputLoc, OutputLoc+ "\\gladius_bec_FileList.txt"});
-            process = Runtime.getRuntime().exec(new String[]{"cmd.exe", "/c" ,"python", location + "bec-tool.py -unpack "+SourceLoc +" "+ OutputLoc + " " + OutputLoc + "\\gladius_bec_FileList.txt"});
-       }catch(Exception e) {
-          System.out.println("Exception Raised" + e.toString());
-       }
-       System.out.println("command loaded?");
-       InputStream stdout = process.getInputStream();
-       BufferedReader reader = new BufferedReader(new InputStreamReader(stdout,StandardCharsets.UTF_8));
-       String line;
+    public void unpackBec(String SourceLoc, String OutputLoc){
        try{
-          while((line = reader.readLine()) != null){
-               System.out.println("stdout: "+ line);
-          }
-       }catch(IOException e){
-             System.out.println("Exception in reading output"+ e.toString());
-       }*/
-        //simpler way?
-        try{
-       String location = gui.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-       String[] cmdArray = new String[6];
-       cmdArray[0] = "python";
-       cmdArray[1] = location + "bec-tool.py";
-       cmdArray[2] = "-unpack";
-       cmdArray[3] = SourceLoc;
-       cmdArray[4] = OutputLoc;
-       cmdArray[5] = "gladius_bec_FileList.txt";
-       Process process = Runtime.getRuntime().exec(cmdArray);
+        String location = gui.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        System.out.println(location);
+        String[] cmdArray = new String[6];
+        cmdArray[0] = "python";
+        cmdArray[1] = location + "bec-tool.py";
+        cmdArray[2] = "-unpack";
+        cmdArray[3] = SourceLoc;
+        cmdArray[4] = OutputLoc;
+        cmdArray[5] = "gladius_bec_FileList.txt";
+        Process process = Runtime.getRuntime().exec(cmdArray);
+        BufferedReader stdInput = new BufferedReader(new 
+         InputStreamReader(process.getInputStream()));
+
+         BufferedReader stdError = new BufferedReader(new 
+              InputStreamReader(process.getErrorStream()));
+
+         // Read the output from the command
+        System.out.println("Here is the standard output of the command:\n");
+        String s = null;
+        while ((s = stdInput.readLine()) != null) {
+            System.out.println(s);
+        }
+
+        // Read any errors from the attempted command
+        System.out.println("Here is the standard error of the command (if any):\n");
+        while ((s = stdError.readLine()) != null) {
+            System.out.println(s);
+        }
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+    
+    public void packBec(String SourceLoc, String OutputLoc){
+               try{
+        String location = gui.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        System.out.println(location);
+        String[] cmdArray = new String[6];
+        cmdArray[0] = "python";
+        cmdArray[1] = location + "bec-tool.py";
+        cmdArray[2] = "-pack";
+        cmdArray[3] = SourceLoc;
+        cmdArray[4] = OutputLoc+"/DATA.BEC";
+        cmdArray[5] = SourceLoc+"gladius_bec_FileList.txt";
+        Process process = Runtime.getRuntime().exec(cmdArray);
+        BufferedReader stdInput = new BufferedReader(new 
+         InputStreamReader(process.getInputStream()));
+
+         BufferedReader stdError = new BufferedReader(new 
+              InputStreamReader(process.getErrorStream()));
+
+         // Read the output from the command
+        System.out.println("Here is the standard output of the command:\n");
+        String s = null;
+        while ((s = stdInput.readLine()) != null) {
+            System.out.println(s);
+        }
+
+        // Read any errors from the attempted command
+        System.out.println("Here is the standard error of the command (if any):\n");
+        while ((s = stdError.readLine()) != null) {
+            System.out.println(s);
+        }
         } catch (Exception ex){
             ex.printStackTrace();
         }
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnExtract;
+    private javax.swing.ButtonGroup btnGrpMode;
+    private javax.swing.ButtonGroup btnGrpTools;
     private javax.swing.JButton btnOutputLoc;
+    private javax.swing.JButton btnRunTool;
     private javax.swing.JButton btnSourceLoc;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JFileChooser jfcOutput;
     private javax.swing.JFileChooser jfcSourceFile;
     private javax.swing.JRadioButton jrbBec;
+    private javax.swing.JRadioButton jrbPack;
     private javax.swing.JRadioButton jrbPak;
     private javax.swing.JRadioButton jrbTok;
+    private javax.swing.JRadioButton jrbUnpack;
     private javax.swing.JRadioButton jrbZlib;
     private javax.swing.JTextField txtOutputLoc;
     private javax.swing.JTextField txtSourceLoc;
